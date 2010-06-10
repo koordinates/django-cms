9e57307defe97b6fedc34eef5b79c909617d8d43
Craig de Stigter <craig.destigter@koordinates.com>
2010-06-10 16:24:49 +1200

    undo regression for extra_context kwarg

--- cms/admin/pageadmin.py
+++ cms/admin/pageadmin.py
@@ -544,7 +544,7 @@ class PageAdmin(model_admin):
                 'show_delete_translation': len(obj.get_languages()) > 1,
                 'current_site_id': settings.SITE_ID,
             }
-            extra_context = self.update_language_tab_context(request, obj, extra_context=extra_context)
+            extra_context = self.update_language_tab_context(request, obj, context=extra_context)
         tab_language = request.GET.get("language", None)
         response = super(PageAdmin, self).change_view(request, object_id, extra_context=extra_context)
 
@@ -719,19 +719,20 @@ class PageAdmin(model_admin):
     def recover_view(self, request, version_id, extra_context=None):
         if not self.has_recover_permission(request):
             raise PermissionDenied
-        extra_context = self.update_language_tab_context(request, None, extra_context=extra_context)
+        extra_context = self.update_language_tab_context(request, None, context=extra_context)
         return super(PageAdmin, self).recover_view(request, version_id, extra_context=extra_context)
     
     def revision_view(self, request, object_id, version_id, extra_context=None):
         if not self.has_change_permission(request, Page.objects.get(pk=object_id)):
             raise PermissionDenied
-        extra_context = self.update_language_tab_context(request, None, extra_context=extra_context)
-        return super(PageAdmin, self).revision_view(request, object_id, version_id, extra_context=extra_context)
-    
+        extra_context = self.update_language_tab_context(request, None, context=extra_context)
+        response = super(PageAdmin, self).revision_view(request, object_id, version_id, extra_context=extra_context)
+        return response
+
     def history_view(self, request, object_id, extra_context=None):
         if not self.has_change_permission(request, Page.objects.get(pk=object_id)):
             raise PermissionDenied
-        extra_context = self.update_language_tab_context(request, None, extra_context=extra_context)
+        extra_context = self.update_language_tab_context(request, None, context=extra_context)
         return super(PageAdmin, self).history_view(request, object_id, extra_context=extra_context)
 
     def render_revision_form(self, request, obj, version, context, revert=False, recover=False):
