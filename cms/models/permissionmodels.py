da83ee611d8608c2c60e0935230e9b008517d67a
Craig de Stigter <craig.destigter@koordinates.com>
2012-10-31 15:08:56 +1300

    compatibility with custom user models

--- cms/models/permissionmodels.py
+++ cms/models/permissionmodels.py
@@ -2,7 +2,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from django.contrib.auth.models import User, Group
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 
 from cms.models import Page, ACCESS_CHOICES, ACCESS_PAGE_AND_DESCENDANTS
@@ -13,9 +15,9 @@ class AbstractPagePermission(models.Model):
     """Abstract page permissions
     """
     # who:
-    user = models.ForeignKey(User, verbose_name=_("user"), blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("user"), blank=True, null=True)
     group = models.ForeignKey(Group, verbose_name=_("group"), blank=True, null=True)
-    
+
     # what:
     can_change = models.BooleanField(_("can edit"), default=True)
     can_add = models.BooleanField(_("can add"), default=True)
@@ -26,18 +28,18 @@ class AbstractPagePermission(models.Model):
     can_move_page = models.BooleanField(_("can move"), default=True)
     can_moderate = models.BooleanField(_("can moderate"), default=True)
     can_view = models.BooleanField(_("view restricted"), default=False, help_text=_("frontend view restriction"))
-    
+
     class Meta:
         abstract = True
         app_label = 'cms'
-    
+
     @property
     def audience(self):
         """Return audience by priority, so: All or User, Group
         """
         targets = filter(lambda item: item, (self.user, self.group,))
         return ", ".join([unicode(t) for t in targets]) or 'No one'
-    
+
     def save(self, *args, **kwargs):
         if not self.user and not self.group:
             # don't allow `empty` objects
@@ -50,41 +52,41 @@ class GlobalPagePermission(AbstractPagePermission):
     """
     can_recover_page = models.BooleanField(_("can recover pages"), default=True, help_text=_("can recover any deleted page"))
     sites = models.ManyToManyField(Site, null=True, blank=True, help_text=_('If none selected, user haves granted permissions to all sites.'), verbose_name=_('sites'))
-    
+
     objects = BasicPagePermissionManager()
-    
+
     class Meta:
         verbose_name = _('Page global permission')
         verbose_name_plural = _('Pages global permissions')
         app_label = 'cms'
-    
+
     def __unicode__(self):
         return "%s :: GLOBAL" % self.audience
 
 
 class PagePermission(AbstractPagePermission):
     """Page permissions for single page
-    """ 
+    """
     grant_on = models.IntegerField(_("Grant on"), choices=ACCESS_CHOICES, default=ACCESS_PAGE_AND_DESCENDANTS)
     page = models.ForeignKey(Page, null=True, blank=True, verbose_name=_("page"))
-    
+
     objects = PagePermissionManager()
-    
+
     class Meta:
         verbose_name = _('Page permission')
         verbose_name_plural = _('Page permissions')
         app_label = 'cms'
-    
+
     def __unicode__(self):
         page = self.page_id and unicode(self.page) or "None"
         return "%s :: %s has: %s" % (page, self.audience, unicode(dict(ACCESS_CHOICES)[self.grant_on]))
 
 
-class PageUser(User):
+class PageUser(get_user_model()):
     """Cms specific user data, required for permission system
     """
-    created_by = models.ForeignKey(User, related_name="created_users")
-    
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="created_users")
+
     class Meta:
         verbose_name = _('User (page)')
         verbose_name_plural = _('Users (page)')
@@ -92,10 +94,10 @@ class PageUser(User):
 
 
 class PageUserGroup(Group):
-    """Cms specific group data, required for permission system 
+    """Cms specific group data, required for permission system
     """
-    created_by = models.ForeignKey(User, related_name="created_usergroups")
-    
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="created_usergroups")
+
     class Meta:
         verbose_name = _('User group (page)')
         verbose_name_plural = _('User groups (page)')
