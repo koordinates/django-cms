3d6b5c7d405d969ffbf63e76fd9ed428175a16ce
Craig de Stigter <craig.destigter@koordinates.com>
2012-11-28 13:06:26 +1300

    fix parsing of uppercase tag names from tinymce in IE

--- cms/utils/html.py
+++ cms/utils/html.py
@@ -2,13 +2,27 @@
 from html5lib import sanitizer, serializer, treebuilders, treewalkers
 import html5lib
 
-DEFAULT_PARSER = html5lib.HTMLParser(tokenizer=sanitizer.HTMLSanitizer,
+
+# HACK: somehow the html5lib parses uppercase tags as text elements.
+# (see http://code.google.com/p/html5lib/issues/detail?id=47 )
+# So we force the tokenizer to lowercase them first.
+class BetterSanitizer(sanitizer.HTMLSanitizer):
+    def __init__(self, *args, **kwargs):
+        kwargs.update({
+            "lowercaseElementName": True,
+            "lowercaseAttrName": True,
+        })
+        super(BetterSanitizer, self).__init__(*args, **kwargs)
+
+
+DEFAULT_PARSER = html5lib.HTMLParser(tokenizer=BetterSanitizer,
                                      tree=treebuilders.getTreeBuilder("dom"))
+
 
 def clean_html(data, full=True, parser=DEFAULT_PARSER):
     """
     Cleans HTML from XSS vulnerabilities using html5lib
-    
+
     If full is False, only the contents inside <body> will be returned (without
     the <body> tags).
     """
